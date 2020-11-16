85bd9db24828536789c1ec19a702cb9867f2c231
SoniaALLSauces <sonia.antlla@educa.jcyl.es>
2020-11-16 09:39:32 +0100

    cambio Sonia

---
+++ sonia.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo '<h1>Hola Soy Sonia</h1>';
+echo '<h2><a href="index.php">Index Susana</a></h2>';
+echo '<h2><a href="Cristina.php">cristina</a></h2>';
