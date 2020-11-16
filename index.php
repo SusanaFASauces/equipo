2f096d1e7eb1cbf3430014fcd2956d1a888d6524
Susana FA <susana.fabant@educa.jcyl.es>
2020-11-16 09:46:23 +0100

    index.php creado por el master

--- index.php
+++ index.php
@@ -5,4 +5,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "Hola mundo, soy Susana";
+echo "<h1>Hola soy Susana</h1>";
+echo "<h2><a href=sonia.php>SoniaALLSauces</a></h2>";
+echo "<h2><a href=cristina.php>CristinaMLSauces</a></h2>";
